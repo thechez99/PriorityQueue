1f0e298ed2463716aa448409c669cf04870fe837
chester@chestserroberts.co.uk <Alkhobar6273~>
2022-03-23 15:38:32 +0000

    Sets up SLPQ

---
+++ src/queuemanager/UnsortedArrayPriorityQueue.java
@@ -0,0 +1,86 @@
+package queuemanager;
+
+/* Using many constructs from the SortedArrayPriorityQueue
+ * as a template while figuring how this class will work
+ * */
+
+public class UnsortedArrayPriorityQueue<T> implements PriorityQueue<T> {
+
+    /* Class Variables */
+
+    //Data storage
+    private final Object[] storage;
+
+    //Size of array
+    private final int capactiy;
+
+    //Index location of the last object in queue
+    private int tailIndex;
+
+
+    /* Class Constructor */
+    public UnsortedArrayPriorityQueue(int size){
+        storage = new Object[size];
+        capactiy = size;
+        tailIndex = -1;
+    }
+
+    /* Class Functions
+     * Overridden functions from interface PriorityQueue  */
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+
+        tailIndex++;
+        if (tailIndex >= capactiy){
+            tailIndex--;
+            throw new QueueOverflowException();
+        } else{
+            storage[tailIndex] = new PriorityItem<>(item, priority);
+        }
+
+    }
+
+    /* Function that returns the highest priority item in the queue */
+    @Override
+    public T head() throws QueueUnderflowException {
+        if(isEmpty()){
+            throw new QueueUnderflowException();
+        } else{
+            int pos = this.getHighestPriority();
+            return ((PriorityItem<T>) storage[pos]).getItem();
+        }
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+        if (isEmpty()){
+            throw new QueueUnderflowException();
+        } else{
+            int pos = this.getHighestPriority();
+
+
+
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+
+    public int getHighestPriority(){
+        int highestPriority = -1;
+        int pos = -1;
+
+        for (int i = 0; i < capactiy+1; i++){
+            if(((PriorityItem) storage[i]).getPriority() > highestPriority){
+                highestPriority = ((PriorityItem<T>) storage[i]).getPriority();
+                pos = i;
+            }
+        }
+        return pos;
+    }
+
+}
