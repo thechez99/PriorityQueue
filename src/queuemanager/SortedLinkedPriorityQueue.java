12a85100190f799e434d5a48fb77c93763297a33
chester@chestserroberts.co.uk <Alkhobar6273~>
2022-03-24 01:32:27 +0000

    Sets up ULPQ

--- src/queuemanager/SortedLinkedPriorityQueue.java
+++ src/queuemanager/SortedLinkedPriorityQueue.java
@@ -1,4 +1,52 @@
 package queuemanager;
 
-public class SortedLinkedPriorityQueue {
+public class SortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+
+    /* Class Variables */
+
+    private final Object[] storage;
+
+    private final int cap;
+
+    private int tailIndex;
+
+    /* CLass Constructor(s) */
+
+    public SortedLinkedPriorityQueue(int size){
+        storage = new Object[size];
+        cap = size;
+        tailIndex = -1;
+    }
+
+    /* Class Override Function(s) */
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+
+
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        return null;
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /* Class Functions */
+
 }
+
+/* CODE REFERENCES
+*
+*
+*
+* */
--- src/queuemanager/UnsortedLinkedPriorityQueue.java
+++ src/queuemanager/UnsortedLinkedPriorityQueue.java
@@ -1,4 +1,76 @@
 package queuemanager;
 
-public class UnsortedLinkedPriorityQueue {
+public class UnsortedLinkedPriorityQueue<T> implements PriorityQueue<T> {
+
+    /* Class Variables */
+
+    private Node headNode;
+
+    /* Class Constructor(s) */
+
+    public UnsortedLinkedPriorityQueue(){
+
+    }
+
+    /* Class Override Function(s) */
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        return null;
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /* Class Function(s) */
+
+    /* Supporting Class Declarations */
+    class Node{
+
+        private PriorityItem<T> nodeData;
+        private int nextNode;
+
+        /* Node Constructor */
+
+        public Node(PriorityItem<T> priorityItem){
+            this.nodeData = priorityItem;
+        }
+
+        /* Node Getter and Setters */
+
+        public PriorityItem<T> getNodeData() {
+            return nodeData;
+        }
+
+        public void setNodeData(PriorityItem<T> pI){
+            this.nodeData = pI;
+        }
+
+        public int getNextNode() {
+            return nextNode;
+        }
+
+        public void setNextNode(int nextNode){
+            this.nextNode = nextNode;
+        }
+
+    }
+
 }
+
+
+/* CODE REFERENCES
+* Dixit, R. (2017) Introduction to Data Structures & Algorithms in Java [online]. LinkedIn Learning. Available at <https://www.linkedin.com/learning/introduction-to-data-structures-algorithms-in-java/what-is-a-linked-list?autoplay=true&resume=false&u=2102796> [24 March 2022]
+*/
