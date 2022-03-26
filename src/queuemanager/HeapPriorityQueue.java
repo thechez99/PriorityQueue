d5fdf5b78b0bb5577787da8edbcf417f472c2275
chester@chestserroberts.co.uk <Alkhobar6273~>
2022-03-26 22:23:19 +0000

    Final Commit

--- src/queuemanager/HeapPriorityQueue.java
+++ src/queuemanager/HeapPriorityQueue.java
@@ -1,4 +1,37 @@
 package queuemanager;
 
-public class HeapPriorityQueue {
+public class HeapPriorityQueue<T> implements PriorityQueue<T>{
+
+
+    /* Class Variables */
+
+    /* Class Constructor(s) */
+
+    /* Class Override Functions */
+
+    @Override
+    public void add(T item, int priority) throws QueueOverflowException {
+
+    }
+
+    @Override
+    public T head() throws QueueUnderflowException {
+        return null;
+    }
+
+    @Override
+    public void remove() throws QueueUnderflowException {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /* Class Functions */
+
+    /* Supplemental Classes */
+
+
 }
